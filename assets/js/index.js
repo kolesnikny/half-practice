30859f9b676d188930c270aaf8acabb79d2949e6
Mykola Kolisnyk <kolesnikny@gmail.com>
2022-02-01 19:42:13 +0200

    Initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/*1 */
+function getSum(array, numberElements) {
+    if (numberElements <= 0 || numberElements > array.length) {
+        throw new RangeError('bed number');
+    }
+    let sum = 0;
+    for (let i = 0; i < numberElements; i++) {
+        sum += array[i];
+    }
+    return sum;
+}
+
+//console.log(getSum([1, 2, 3, 4, 5, 6, 7], 6));
+/*2 */
+const Student ={
+    'name' : 
+} 
+
+class/* */
+/* */
